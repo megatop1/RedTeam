e745c29b39df336b112085c363b77b86de1f2b63
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-10-10 23:06:10 -0400

    Update InstallTools.sh

--- Windows/InstallTools.sh
+++ Windows/InstallTools.sh
@@ -6,18 +6,18 @@ sudo gem install evil-winrm
 sudo git clone https://github.com/Hackplayers/evil-winrm.git
 
 # Install Covenant C2
-git clone --recurse-submodules https://github.com/cobbr/Covenant
+sudo git clone --recurse-submodules https://github.com/cobbr/Covenant
 
-wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
+sudo rm -f packages-microsoft-prod.deb
 
 sudo apt-get update
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install dotnet-sdk-3.1
+sudo apt-get -y install dotnet-sdk-3.1
 
 cd Covenant/Covenant
-dotnet run
+sudo dotnet run
 
 # Install Shellter 
