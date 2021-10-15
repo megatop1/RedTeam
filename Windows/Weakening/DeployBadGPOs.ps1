9d3b597c3e4a0cff6a0b057dbcdc0125166ecd65
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-10-15 09:28:01 -0400

    Create DeployBadGPOs.ps1

---
+++ Windows/Weakening/DeployBadGPOs.ps1
@@ -0,0 +1,13 @@
+# This file must be deployed on a domain controller
+
+# GPO1: Disable Firewall accross the domain 
+
+# GPO2: Enable SMB1 Accross the domain and disable SMB2 and SMB3
+
+# Enable Guest Account
+
+# Enable Windows to Store LAN Manager Hash
+
+# Enable Print-Spooler Service
+
+# Disable Windows Defender
