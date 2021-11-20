c02e40c60a6c5afef8738e5a03563d69a6918788
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-11-19 20:54:42 -0500

    tested EnableLanManagerHash

--- Windows/Weakening/DeployBadGPOs.ps1
+++ Windows/Weakening/DeployBadGPOs.ps1
@@ -50,7 +50,7 @@ New-GPLink -name "EnableSMB1" -Target $target -LinkEnabled Yes
 
 # Enable Windows to Store LAN Manager Hash
 New-GPO -name "StoreLanManagerHash" -domain $domainName
-Set-GPRegistryValue -name "StoreLanManagerHash" -key "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa" -ValueName "NoLmHash" -type DWORD -Value 0
+Set-GPRegistryValue -name "StoreLanManagerHash" -key "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" -ValueName "NoLmHash" -type DWORD -Value 0
 New-GPLink -name "StoreLanManagerHash" -Target $target -LinkEnabled Yes 
 
 # Enable Print-Spooler Service
