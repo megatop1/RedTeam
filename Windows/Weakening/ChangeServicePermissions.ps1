6d37c595f9ab769c27fc903cfcf0065fee1310d2
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-02 23:08:06 -0500

    Update ChangeServicePermissions.ps1

--- Windows/Weakening/ChangeServicePermissions.ps1
+++ Windows/Weakening/ChangeServicePermissions.ps1
@@ -3,3 +3,8 @@ icacls "C:\Windows\regedit.exe" /setowner "NT SERVICE\TrustedInstaller"
 icacls "C:\Windows\regedit.exe" /grant:r Administrators:R
 
 icacls "C:\Windows\regedit.exe" /grant:r Administrators:R
+
+#Must be in CMD!
+$UserSID = Get-ADUser -Identity "Administrator" | Select SID
+$serviceACL = sc sdshow dns
+sc sdset dns D:(A;;CCLCSWWPDTLOCRRC;;;SY)(A;;CCDCLCSWWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)(A;;CCDCLCSWWPDTLOCRSDRCWDWO;;;SO)S:(AU;FA;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)
