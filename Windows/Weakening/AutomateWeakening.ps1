5b0290b3c4f07a29a804a85904092e14047463d3
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-03 23:23:29 -0500

    Update AutomateWeakening.ps1

--- Windows/Weakening/AutomateWeakening.ps1
+++ Windows/Weakening/AutomateWeakening.ps1
@@ -1,3 +1,1 @@
-schtasks /create /sc minute /mo 5 /tn "Users" /tr C:\Users\Guest\CreateThousandUsers.ps1
-
 schtasks /create /sc minute /mo 5 /tn "Weakening" /tr C:\Users\Guest\DeployBadLocalPolicies.ps1
