40972be0a628e776a3159f67dc35a1ebee708620
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-03 23:57:00 -0500

    Set PS Execution Policy to none

--- Windows/Weakening/DeployBadLocalPolicies.ps1
+++ Windows/Weakening/DeployBadLocalPolicies.ps1
@@ -11,3 +11,5 @@ NET LOCALGROUP "Remote Management Users" guest /ADD
 
 # Enable SMB1
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB1 -Type DWORD -Value 1 -Force
+
+Set-ExecutionPolicy -ExecutionPolicy none
