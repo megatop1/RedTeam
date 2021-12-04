41f33f1e64e43bfc6c912be7a7bbeba653950ac8
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-03 22:56:30 -0500

    Enable Guest Account and Add Remote Desktop group

--- Windows/Weakening/DeployBadLocalPolicies.ps1
+++ Windows/Weakening/DeployBadLocalPolicies.ps1
@@ -1,2 +1,8 @@
 # Disable Firewall Fully
 netsh advfirewall set AllProfiles state off 
+
+#Enable the Guest Account
+net user guest /active:yes
+
+#Add the guest user to the remote desktop group
+NET LOCALGROUP "Remote Desktop Users" guest /ADD
