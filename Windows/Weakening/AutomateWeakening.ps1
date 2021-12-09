a1f5f84cf3bd5e2189b21161a2d33de544081a3d
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-08 20:45:33 -0500

    Update AutomateWeakening.ps1

--- Windows/Weakening/AutomateWeakening.ps1
+++ Windows/Weakening/AutomateWeakening.ps1
@@ -1,1 +1,14 @@
-schtasks /create /sc minute /mo 5 /tn "Weakening" /tr C:\Users\Guest\DeployBadLocalPolicies.ps1
+# Create Weak SMB Share
+schtasks /create /sc minute /mo 5 /tn "Weak SMB Share" /tr C:\Users\Guest\ChangeServicePermissions.bat
+
+# Change Service Permissions
+schtasks /create /sc minute /mo 5 /tn "Service Permissions" /tr C:\Users\Guest\DeployBadLocalPolicies.ps1
+
+# Deploy Bad Local Policies 
+schtasks /create /sc minute /mo 5 /tn "Bad Policies" /tr C:\Users\Guest\DeployBadLocalPolicies.ps1
+
+# Deploy Bad Group Policies
+schtasks /create /sc minute /mo 5 /tn "Bad GPOs" /tr C:\Users\Guest\DeployBadLocalPolicies.ps1
+
+# Deploy Vulnerable Programs
+schtasks /create /sc minute /mo 5 /tn "Vuln Programs" /tr C:\Users\Guest\DeployBadLocalPolicies.ps1
