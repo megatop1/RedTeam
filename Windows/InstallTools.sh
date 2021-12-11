09b8450d2741066b28a6eae2669e07d77f988b63
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-10 22:22:38 -0500

    Update InstallTools.sh

--- Windows/InstallTools.sh
+++ Windows/InstallTools.sh
@@ -44,7 +44,7 @@ sudo git clone https://github.com/hasherezade/process_ghosting
 #sudo dotnet run
 
 # PowerSploit
-git clone https://github.com/PowerShellMafia/PowerSploit
+sudo git clone https://github.com/PowerShellMafia/PowerSploit
 
 # Undetectable Payloads
-https://github.com/youhacker55/PayGen.git
+sudo git clone https://github.com/youhacker55/PayGen.git
