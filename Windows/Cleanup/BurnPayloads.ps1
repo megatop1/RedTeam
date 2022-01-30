e20cf329346cabe0ca74141042d51959cccc1206
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-30 04:12:02 -0500

    Update BurnPayloads.ps1

--- Windows/Cleanup/BurnPayloads.ps1
+++ Windows/Cleanup/BurnPayloads.ps1
@@ -1,6 +1,6 @@
 # The following script will remove all of the payloads on the system in case the system is being investigated by blue team and we need to cover our tracks
 del C:\Windows\System32\malware.xml
-del C:\Program Files\Common Files\System\iexplorer.exe
+del "C:\Program Files\Common Files\System\iexplorer.exe"
 del C:\Windows\System32\PowerHell.bat
 del C:\Program Files\Mozilla Firefox\PowerManagement.exe
 del C:\Users\Public\VMWareTools.exe
