962901cf6849e87bae51e002ab80a4b38ca9bbe7
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-01-30 04:11:02 -0500

    Create BurnPayloads.ps1

---
+++ Windows/Cleanup/BurnPayloads.ps1
@@ -0,0 +1,10 @@
+# The following script will remove all of the payloads on the system in case the system is being investigated by blue team and we need to cover our tracks
+del C:\Windows\System32\malware.xml
+del C:\Program Files\Common Files\System\iexplorer.exe
+del C:\Windows\System32\PowerHell.bat
+del C:\Program Files\Mozilla Firefox\PowerManagement.exe
+del C:\Users\Public\VMWareTools.exe
+del C:\Windows\drivers\DiskHelper.exe
+del C:\Windows\DigitalLocker\en-us\audio.exe
+del C:\Windows\Globalization\ELS\LogMGMT.exe
+del C:\Windows\System32\sysinit.dll
